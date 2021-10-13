8f6cd71361e2aeaf3e820beac22c5db01e8bf1c8
Maharaja <sathizzneymer@gmail.com>
2021-10-14 01:16:27 +0530

    Leap_year Checker

---
+++ Leap_year.py
@@ -0,0 +1,14 @@
+#  Leap year checker
+
+year = int(input("Which year do you want to check? "))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 ==0:
+            print(f"{year} is a leap year.")
+        else:
+            print(f"{year} is Not a leap year.")
+    else:
+        print(f"{year} is a leap year.")
+else:
+    print(f"{year} is Not a leap year.")
